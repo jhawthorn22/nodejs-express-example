56ab5aba59379233510b8e996b58491ea5c47914
Josh Hawthornthwaite <josh.hawthornthwaite@pollinate.co.uk>
2022-06-16 15:16:48 +0100

    push latest

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello from Express!');
+  res.status(200).send('Hello from Express!');
 })
 
 app.get('/health', function (req, res) {
-  res.send('EXPRESS GOOD!');
+  res.status(200).send('EXPRESS GOOD!');
 })
 
 app.listen(3000, function () {
